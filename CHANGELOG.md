bb992441a4182c03bd0bcbcc09273017d497de14
Jonathan Knapp <jon@coffeeandcode.com>
2019-10-13 17:10:04 -0400

    Remove assert_cmd and rewrite test to only use std

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [unreleased] - `master` branch
 
-Nothing so far.
+### Removed
+
+- removed `assert_cmd` dependency
 
 ## [0.2.0] - 2019-10-13
 
--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -1,12 +1,17 @@
-use assert_cmd::prelude::*;
 use std::process::Command;
+use std::str;
 
 #[test]
 fn it_will_output_to_stderr_if_git_not_found() {
-    Command::main_binary()
-        .unwrap()
+    let binary_name = env!("CARGO_PKG_NAME");
+    let output = Command::new(format!("./target/debug/{}", binary_name))
         .env("PATH", "")
-        .assert()
-        .stderr("Unable to find git on your system.\n")
-        .code(1);
+        .output()
+        .expect("failed to execute process");
+
+    assert_eq!(1, output.status.code().unwrap());
+    assert_eq!(
+        "Unable to find git on your system.\n",
+        str::from_utf8(&output.stderr).unwrap()
+    );
 }
